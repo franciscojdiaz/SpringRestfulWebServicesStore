14b4964d480def634f66649383f2e7401388aa2b
franciscojdiaz <franciscodiaz3@gmail.com>
2021-08-25 11:56:21 -0400

    Delete service-customer directory

--- service-customer/src/main/java/com/academy/store/customer/servicecustomer/repository/RegionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.academy.store.customer.servicecustomer.repository;
-
-import com.academy.store.customer.servicecustomer.entity.Region;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RegionRepository extends JpaRepository<Region, Long> {
-
-
-}
